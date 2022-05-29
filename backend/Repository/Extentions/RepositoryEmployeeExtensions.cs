cd0d9c6db47d91bc690ce67f8aed602c0c0c1d01
Mohammad Nour Rezek <40129269+Mohammad-Nour-Rezek@users.noreply.github.com>
2022-05-29 16:17:59 +0300

    Finish Searching with test for Employees

---
+++ backend/Repository/Extentions/RepositoryEmployeeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extentions
+{
+    public static class RepositoryEmployeeExtensions
+    {
+        public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge) => 
+            employees.Where(c => (c.Age >= minAge && c.Age <= maxAge));
+
+        public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return employees;
+            
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
+            
+            return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+            
+    }
+}
